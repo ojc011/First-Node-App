d769e404145ec82c12ebac526404dde42167c46c
Oliver C <ocronk10@gmail.com>
2022-03-28 18:37:39 -0500

    Syncing remote and local repos

--- app.js
+++ app.js
@@ -1,50 +1,50 @@
-const http = require('http')
-const {readFileSync} = require('fs');
+const http = require("http");
+const { readFileSync } = require("fs");
 
-const homePage = readFileSync('./index.html')
-const homeStyles = readFileSync('./styles.css')
-const homeLogo = readFileSync('./logo.svg')
-const homeLogic = readFileSync('./index.html')
+const homePage = readFileSync("./index.html");
+const homeStyles = readFileSync("./styles.css");
+const homeLogo = readFileSync("./logo.svg");
+const homeLogic = readFileSync("./index.html");
 
-const server = http.createServer((req,res) => {
-  // console.log(req.url)  
+const server = http.createServer((req, res) => {
+  // console.log(req.url)
   const url = req.url;
   //home page
-  if(url === '/'){
-    res.writeHead(200,{ 'content-type': 'text/html' }) 
-    res.write(homePage)
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(homePage);
     res.end();
-}
-//about page
-else if(url === '/about'){
-    res.writeHead(200,{ 'content-type': 'text/html' }) 
-    res.write('<h1>About Page</h1>')
+  }
+  //about page
+  else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>About Page</h1>");
     res.end();
-}
-//style page
-else if(url === '/styles.css'){
-    res.writeHead(200,{ 'content-type': 'text/css' }) 
-    res.write(homeStyles)
+  }
+  //style page
+  else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
     res.end();
-}
-//logo
-else if(url === '/logo.svg'){
-    res.writeHead(200,{ 'content-type': 'image/svg+xml' }) 
-    res.write(homeLogo)
+  }
+  //logo
+  else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(homeLogo);
     res.end();
-}
-//logic
-else if(url === '/browser-app.js'){
-    res.writeHead(200,{ 'content-type': 'text/javascript' }) 
-    res.write(homeLogic)
+  }
+  //logic
+  else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(homeLogic);
     res.end();
-}
-//404
-else{
-    res.writeHead(404, { 'content-type': 'text/html' })
-    res.write( '<h1>PAGE NOT FOUND</h1>')
-    res.end()
-    }
-})
+  }
+  //404
+  else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>PAGE NOT FOUND</h1>");
+    res.end();
+  }
+});
 
-server.listen(5000)
+server.listen(5000);
